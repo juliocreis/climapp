3b973f5091a760890ea636a535bbea8175972c1f
juliocreis <julioreis951@gmail.com>
2026-06-08 22:04:23 -0300

    feat: consumir api de tempo

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,43 @@
 import SearchBar from "./components/SearchBar";
 import WeatherCard from "./components/WeatherCard";
 import "./App.css";
+import { useEffect, useState } from "react";
+
+const API_KEY = import.meta.env.VITE_WEATHER_API_KEY;
 
 function App() {
+  const [weather, setWeather] = useState(null);
+
+  useEffect(() => {
+    async function fetchWeather() {
+      try {
+        const response = await fetch(
+          `https://api.hgbrasil.com/weather?format=json-cors&key=${API_KEY}&git city_name=Imperatriz, MA`,
+        );
+        const data = await response.json();
+
+        if (data.results) {
+          setWeather(data.results);
+        }
+      } catch (erro) {
+        console.error("Erro na busca pela API:", erro);
+      }
+    }
+
+    fetchWeather();
+  }, []);
+
   return (
     <div className="app-container">
       <SearchBar />
-      <h1>São Paulo. SP</h1>
-      <WeatherCard />
+      
+      {weather && (
+        <>
+          <h1>{weather.city}</h1>
+          <WeatherCard weather={weather} />
+        </>
+      )}
+
     </div>
   );
 }
