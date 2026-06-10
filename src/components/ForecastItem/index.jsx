da1fd915c8bd01b7acf1cbb80abf0e08d6aed7e4
juliocreis <julioreis951@gmail.com>
2026-06-10 12:31:01 -0300

    feat: formata dia da semana na previsao dos proximos dias

--- src/components/ForecastItem/index.jsx
+++ src/components/ForecastItem/index.jsx
@@ -1,10 +1,20 @@
 import "./styles.css";
 
-const ForecastItem = ({ date, weekday, condition, description, min, max }) => {
+const getWeekday = (dateString) => {
+    const [day, month] = dateString.split('/'); // Separa dia e mês
+    const dateObj = new Date(new Date().getFullYear(), month - 1, day); // Gera data de hoje 
+    const weekday = Intl.DateTimeFormat("pt-BR", { weekday: 'long'}).format(dateObj); // Formata a data para o dia da semana
+
+
+    return weekday.charAt(0).toUpperCase() + weekday.split("-")[0].slice(1); // Torna a primeira letra maiuscula, remove o "-feira" e o "q" minúsculo 
+}
+
+const ForecastItem = ({ date, condition, description, min, max }) => {
+
   return (
     <section className="forecast-card">
       <div className="forecast-day">
-        <p className="forecast-weekday">{weekday}</p>
+        <p className="forecast-weekday">{getWeekday(date)}</p>
         <p className="forecast-date">({date})</p>
       </div>
       <img
