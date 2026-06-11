e26e2ff40b694c6bc12ddad6586132edfff8b8f9
juliocreis <julioreis951@gmail.com>
2026-06-11 14:53:29 -0300

    feat: adiciona funcionalidade de pesquisa

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,26 @@ import WeatherCard from "./components/WeatherCard";
 import "./App.css";
 import { useEffect, useState } from "react";
 import ForecastList from "./components/ForecastList";
+import Loading from "./components/Loading";
 
 const API_KEY = import.meta.env.VITE_WEATHER_API_KEY;
 
 function App() {
   const [weather, setWeather] = useState(null);
   const [forecast, setForecast] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [city, setCity] = useState("");
 
+  const newCity = (city) => {
+    setCity(city);
+  }
+  
   useEffect(() => {
     async function fetchWeather() {
+      setLoading(true);
       try {
         const response = await fetch(
-          `https://api.hgbrasil.com/weather?format=json-cors&key=${API_KEY}&city_name=Imperatriz, MA`,
+          `https://api.hgbrasil.com/weather?format=json-cors&key=${API_KEY}&city_name=${city}`,
         );
         const data = await response.json();
 
@@ -24,17 +32,26 @@ function App() {
         }
       } catch (erro) {
         console.error("Erro na busca pela API:", erro);
+        setLoading(false)
+      } finally {
+        setLoading(false)
       }
     }
 
     fetchWeather();
-  }, []);
+  }, [city]);
 
   return (
     <div className="app-container">
-      <SearchBar />
-
-      {weather && (
+      <SearchBar 
+        city={city} 
+        searchCity={(city) => {
+          newCity(city);
+        }}
+      />
+      {loading ? (
+        <Loading />
+      ) : weather && (
         <>
           <h1>{weather.city}</h1>
           <WeatherCard weather={weather} />
--- src/components/SearchBar/index.jsx
+++ src/components/SearchBar/index.jsx
@@ -1,8 +1,14 @@
 import "./styles.css";
 
-const SearchBar = () => {
+const SearchBar = ({ searchCity }) => {  
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    searchCity(event.target[0].value);
+  }
+
   return (
-    <form className="form-search">
+    <form className="form-search" onSubmit={onSubmit}>
       <label className="search-bar">
         <input type="text" placeholder="Digite a cidade" />
         <button type="submit">
